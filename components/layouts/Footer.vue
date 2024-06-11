f4ae7972130c062ef3cd18b17fe673631cd04488
QuentinHsu <xuquentinyang@gmail.com>
2024-06-11 13:22:22 +0800

    chore: add footer with author information and GitHub link

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -3,5 +3,16 @@
 </script>
 
 <template>
-  <section class="md:pt-6" />
+  <section class="md:pt-6">
+    <footer class="container mx-auto px-4 py-8 text-center text-sm text-gray-500 dark:text-gray-400">
+      <p>
+        Made with by QuentinHsu
+      </p>
+      <p>
+        <a href="https://github.com/QuentinHsu" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </p>
+    </footer>
+  </section>
 </template>
