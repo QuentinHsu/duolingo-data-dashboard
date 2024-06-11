2b55a40330669b8091b043e38875f546bc2e29ed
QuentinHsu <xuquentinyang@gmail.com>
2024-06-11 16:26:53 +0800

    chore: update footer styling and layout

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <section class="md:pt-6">
-    <footer class="container mx-auto px-4 py-8 text-center text-sm text-gray-500 dark:text-gray-400">
+    <footer class="container mx-auto px-4 py-1 text-center text-sm text-gray-500 dark:text-gray-400">
       <p>
         Made with by QuentinHsu
       </p>
