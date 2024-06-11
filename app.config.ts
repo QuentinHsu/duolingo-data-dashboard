1005c6b4c0fc623f834a891590d85271c81c8885
QuentinHsu <xuquentinyang@gmail.com>
2024-06-11 10:58:27 +0800

    chore: update app slogan to mention Cloudflare

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
 export default defineAppConfig({
   title: 'Duolingo Data Dashboard',
-  slogan: 'Track Your Learning Progress, Secure Self-Deployment, All in the Cloud',
+  slogan: 'Track Your Learning Progress, Secure Self-Deployment, All in the Cloudflare',
 })
